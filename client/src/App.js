18fa2dbd69e846e8185dcefd2895b9e3b56c4813
Saner Cakir <sanercakir@gmail.com>
2022-10-08 23:06:22 -0700

    client and server init

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+function App() {
+    return (
+        <div className="App">
+            <p className="text-danger">Hello World</p>
+            <img src={logo} alt="logo" className="img"/>
+      </div>
+    );
+}
+
+export default App;
