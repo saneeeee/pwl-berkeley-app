5b4a27e7f58e19a59bd5bd144598dc5906fb7069
Saner Cakir <sanercakir@gmail.com>
2022-10-10 16:46:37 -0700

    navbar & SVG logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+import logo from './static/SVG/pwl@berkeley-logo.svg';
 function App() {
     return (
         <div className="App">
-            <p className="text-danger">Hello World</p>
-            <img src={logo} alt="logo" className="img"/>
-      </div>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#zbab">
+                        <img src={logo} alt="Logo" width="30" height="24"
+                             className="d-inline-block align-text-top"
+                             style={{width: "60px"}}/>
+                            PWL@Berkeley
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#zbab">Home</a>
+                            <a className="nav-link" href="#zbab">Features</a>
+                            <a className="nav-link" href="#zbab">Pricing</a>
+                            <a className="nav-link disabled">Disabled</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
     );
 }
 
