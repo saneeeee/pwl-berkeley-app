9b6d20172ff0c88d4777c7253e398b67206b4728
Saner Cakir <sanercakir@gmail.com>
2022-10-10 22:27:17 -0700

    static upcoming events UI

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
                     <a className="navbar-brand" href="#zbab">
                         <img src={logo} alt="Logo" width="30" height="24"
                              className="d-inline-block align-text-top"
-                             style={{width: "60px"}}/>
+                             />
                             PWL@Berkeley
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -18,15 +18,59 @@ function App() {
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
                             <a className="nav-link active" aria-current="page" href="#zbab">Home</a>
-                            <a className="nav-link" href="#zbab">Features</a>
-                            <a className="nav-link" href="#zbab">Pricing</a>
-                            <a className="nav-link disabled">Disabled</a>
+                            <a className="nav-link" href="#zbab">About</a>
+                            <a className="nav-link" href="#zbab">Join PWL</a>
                         </div>
                     </div>
                 </div>
             </nav>
+            <div className="container">
+                <h1 className={"text-center my-3"}>Upcoming events</h1>
+                {EventsList()}
+
+
+            </div>
         </div>
     );
+}
+function EventsList() {
+    return (
+        <div className="eventsList">
+            <ul style={{listStyleType: "none"}}>
+                <li>
+                    {EventCard()}
+                </li>
+                <li>
+                    {EventCard()}
+                </li>
+                <li>
+                    {EventCard()}
+                </li>
+                <li>
+                    {EventCard()}
+                </li>
+                <li>
+                    {EventCard()}
+                </li>
+            </ul>
+        </div>
+    );
+}
+function EventCard() {
+    return (
+        <div className="card my-3">
+            <div className="card-body">
+                <h2 className={"card-title"}>Paper Talk: <span className={"text-primary"}>FOO BAR by FUBAR</span></h2>
+                <h4 className={"card-subtitle text-pwl-primary"}>November 1 2022</h4>
+                <p className={"card-text"}>
+                    <strong>Abstract:</strong> In this paper talk we explore laskdjfopw poejghw adfkqwopbjnk as;dg
+                    sdngoiregt qopwrgjnklonqwer qwpojtgn rgq;'o qpowjtjnqwrt pqowjrtlknqw op[qwetkln
+                    qwrntlkwqjrt woptjklqwnme;lwkegkl qp[orjtklonwegkl;as pqowjtklnwqt p;alwejtoijqw
+                </p>
+            </div>
+        </div>
+    );
+
 }
 
 export default App;
