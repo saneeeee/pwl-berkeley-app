72b707b2929a10a29224d7ccfa39ec1251fefdc1
Saner Cakir <sanercakir@gmail.com>
2022-10-24 19:59:36 -0700

    heroku directory fix 2

--- server.js
+++ server.js
@@ -14,7 +14,3 @@ app.listen(PORT, (err) => {
      if (err) return console.log(err);
      console.log(`Server started on port: ${PORT}`);
 });
-app.use(express.static('./client/build'));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
-});
