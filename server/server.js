5a77ddc6af854deb5a45bb627022dddd4d904ac1
Saner Cakir <sanercakir@gmail.com>
2022-10-24 19:38:52 -0700

    express server init & ready for heroku publish

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('../client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+    });
+}
+app.listen(PORT, (err) => {
+     if (err) return console.log(err);
+     console.log(`Server started on port: ${PORT}`);
+});
+app.use(express.static('../client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+});
