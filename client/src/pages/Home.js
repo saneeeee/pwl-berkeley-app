ea1cb1938f96dddb91c58e76ff175fce8982a675
Saner Cakir <sanercakir@gmail.com>
2022-10-27 14:55:45 -0700

    fixed discord link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,12 @@
+import UpcomingEvents from "./UpcomingEvents";
 function Home() {
     return(
-        <div className="text-center">
-            <h1 className="text-pwl-primary">Home Page</h1>
-            <p className="text-primary">Don't know what to say??</p>
+        <div>
+            <div className="text-center">
+                <h1 className="text-pwl-primary">Home Page</h1>
+                <p className="text-primary">Don't know what to say??</p>
+            </div>
+            <UpcomingEvents />
         </div>
     );
 
--- client/src/pages/Join.js
+++ client/src/pages/Join.js
@@ -2,7 +2,6 @@ function Join() {
     return (
         <div className="text-center">
             <h2>Please join our  <a href="https://discord.gg/KNDjrESBvV">discord channel:</a></h2>
-
         </div>
     );
 }
