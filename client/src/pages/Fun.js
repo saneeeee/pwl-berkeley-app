ad080acf582aab9552e3c2544f7265d9337b5b1b
Saner Cakir <sanercakir@gmail.com>
2022-11-03 13:25:11 -0700

    puzzle ammend

--- client/src/pages/Fun.js
+++ client/src/pages/Fun.js
@@ -4,6 +4,22 @@ function Fun() {
         <div className="container">
             <h1 className="text-pwl-primary">Fun?:</h1>
             <h4>Show us your solution to any of the weekly puzzles at one of our events for pizza!!</h4>
+            <Puzzle title="Three Bears">
+                <div>
+                    <p>Your CS professor offers an A+ to anyone who can guess the ages of his 3 bears.
+                        However if you make a single wrong guess, you'll fail the class.</p>
+                    <p>When you asked for a clue, he said: <b>"The product of their ages is 36"</b></p>
+                    <ul>
+                        <li><p>Seeing you still couldn't guess their ages, he gave you another clue:
+                            <b>"The sum of their ages is equal to my office number"</b></p></li>
+                        <li><p>Although you know their office number, you still couldn't guess their ages, so your got a
+                            final clue:<b>"My oldest bear is named Oski"</b></p></li>
+                    </ul>
+                    <p>And you were finally able to guess their ages and grab that sweet A+. <b>How old are the bears?</b></p>
+                    <p><b>Hint:</b> There can be so many factor triples of 36!</p>
+                </div>
+            </Puzzle>
+
             <Puzzle title="Enough Consulting Clubs?">
                 <div>
                     <p>There are <b>n</b> students and <b>k</b> consulting clubs at Berkeley,</p>
@@ -14,22 +30,6 @@ function Fun() {
                     <p>Assuming a consulting club is uniquely identifiable by its members, <b>find an upper bound to the number of consulting clubs on campus.</b></p>
                 </div>
             </Puzzle>
-
-            <Puzzle title="Three Bears">
-                <div>
-                    <p>Your CS professor offers an A+ to anyone who can guess the ages of his 3 bears.
-                        However if you make a single wrong guess, you'll fail the class.</p>
-                        <p>When you asked for a clue, he said: <b>"The product of their ages is 36"</b></p>
-                    <ul>
-                        <li><p>Seeing you still couldn't guess their ages, he gave you another clue:
-                            <b>"The sum of their ages is equal to my office number"</b></p></li>
-                        <li><p>Although you know their office number, you still couldn't guess their ages, so your got a
-                            final clue:<b>"My oldest bear is named Oski"</b></p></li>
-                    </ul>
-                    <p>And you were finally able to guess their ages and grab that sweet A+. <b>How old are the bears?</b></p>
-                </div>
-            </Puzzle>
-
         </div>
     );
 }
