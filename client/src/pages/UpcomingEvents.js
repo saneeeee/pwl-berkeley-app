ca6b2e2bcde84b6d87739a3249b85029ed823954
Saner Cakir <sanercakir@gmail.com>
2022-11-03 11:02:51 -0700

    event details

--- client/src/pages/UpcomingEvents.js
+++ client/src/pages/UpcomingEvents.js
@@ -13,7 +13,8 @@ function EventsList() {
         <div className="eventsList">
             <ul className="px-0" style={{listStyleType: "none"}}>
                 <li>
-                    <EventCard title="Diffuser Cam" researcher=" Eric Markley" date="November 10 2022">
+                    <EventCard title="Diffuser Cam" researcher=" Eric Markley" date="November 10 2022, 6:30pm"
+                               location="Moffitt 103">
                         <>
                             <strong>Abstract:</strong> In this paper talk Eric Markley from Laura Waller's Computaional
                             Imaging Lab will present DiffuserCam: "a lensless camera made of a piece of bumpy plastic,
@@ -21,6 +22,10 @@ function EventsList() {
                             system, the camera is cheap, compact, and easy to build. We’ve demonstrated that our simple
                             system can be used for both 2D photography and 3D image reconstruction from a single
                             acquisition." <a href="https://waller-lab.github.io/DiffuserCam/">Project Page</a>
+                            <ul>
+                                <li>Meet outside Moffitt at 6:30pm for pizza</li>
+                                <li>Talk will start at 7:00pm, Moffit 103</li>
+                            </ul>
                         </>
                     </EventCard>
                 </li>
@@ -34,7 +39,7 @@ function EventCard(props) {
             <div className="card-body">
                 <h2 className={"card-title"}>Paper Talk: <span className={"text-primary"}>{props.title} by
                     {props.researcher}</span></h2>
-                <h4 className={"card-subtitle text-pwl-primary"}>{props.date}</h4>
+                <h4 className={"card-subtitle text-pwl-primary"}>{props.date + " " + props.location}</h4>
                 <p className={"card-text"}>
                     {props.children}
                 </p>
